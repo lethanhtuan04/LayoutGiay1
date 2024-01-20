aeaf944e32c8e634544d4b8e96d678f76b3f1b29
lethanhtuan04 <lethanhtuan040704@gmail.com>
2024-01-20 21:59:06 +0700

    init project

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ViewFlipper;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    ViewFlipper viewFlipper;
+    RecyclerView recyclerViewManHinhChinh;
+    ListView listViewManHinhChinh;
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Anhxa();
+        ActionBar();
+        ActionViewFlipper();
+    }
+
+    private void ActionViewFlipper() {
+        List<String> mangquangcao = new ArrayList<>();
+        mangquangcao.add("https://lambanner.com/wp-content/uploads/2022/10/MNT-DESIGN-BANNER-GIAY-01.jpg");
+        mangquangcao.add("https://lambanner.com/wp-content/uploads/2022/10/MNT-DESIGN-BANNER-GIAY-07.jpg");
+        mangquangcao.add("https://lambanner.com/wp-content/uploads/2022/10/MNT-DESIGN-BANNER-GIAY-08.jpg");
+        for(int i = 0; i<mangquangcao.size();i++){
+            ImageView imageView= new ImageView(getApplicationContext());
+            Glide.with(getApplicationContext()).load(mangquangcao.get(i)).into(imageView);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            viewFlipper.addView(imageView);
+
+
+        }
+        viewFlipper.setFlipInterval(3000);
+        viewFlipper.setAutoStart(true);
+        Animation slide_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_in_right);
+        Animation slide_out = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_out_right);
+        viewFlipper.setInAnimation(slide_in);
+        viewFlipper.setOutAnimation(slide_out);
+    }
+
+    private void ActionBar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(android.R.drawable.ic_menu_sort_by_size);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+
+
+            }
+        });
+    }
+
+    private void Anhxa() {
+        toolbar =findViewById(R.id.toobarmanhinhchinh);
+        viewFlipper=findViewById(R.id.viewlipper);
+        recyclerViewManHinhChinh=findViewById(R.id.recycleview);
+        listViewManHinhChinh=findViewById(R.id.listviewmanhinhchinh);
+        navigationView=findViewById(R.id.navigationview);
+        drawerLayout=findViewById(R.id.drawerlayout);
+
+    }
+}
